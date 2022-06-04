e6bed1d17ee1022a9b4aac80e37cf632169965bc
Viktoria <zaharova181114@gmail.com>
2022-06-04 21:56:22 +0300

    add blocks

--- app/js/common.js
+++ app/js/common.js
@@ -13,3 +13,13 @@ $(window).scroll(function () {
         $('header').removeClass('fixed');
     }
 });
+
+$('.catalog-slider').slick({
+    slidesToShow: 6,
+    variableWidth: true,
+    appendArrows: '.catalog-slider__nav',
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>'
+});
+
+
