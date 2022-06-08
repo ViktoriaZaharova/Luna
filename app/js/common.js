8e21d8c685a7f76a4c2841736376aa456c5451f8
Viktoria <zaharova181114@gmail.com>
2022-06-08 08:25:31 +0300

    add page

--- app/js/common.js
+++ app/js/common.js
@@ -36,6 +36,51 @@ $('.personalization-slider').slick({
     nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#right"></use></svg></button>'
 });
 
+$('.product-gallery-preview').slick({
+    slidesToShow: 4,
+    vertical: true,
+    verticalSwiping: true,
+    focusOnSelect: true,
+    asNavFor: '.product-gallery-max',
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#right"></use></svg></button>',
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                vertical: false,
+                verticalSwiping: false,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 3,
+                vertical: false,
+                verticalSwiping: false,
+            }
+        },
+        {
+            breakpoint: 420,
+            settings: {
+                slidesToShow: 2,
+                vertical: false,
+                verticalSwiping: false,
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+
+$('.product-gallery-max').slick({
+    slidesToShow: 1,
+    fade: true,
+    arrows: false
+});
+
+
 $('.btn-burger').on('click', function (e) {
     e.preventDefault();
    $('.mobile-menu').fadeToggle();
